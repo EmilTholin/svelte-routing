2b4e2a0223e134eaf4c2877ae04bcf6c16c3dc63
Emil Tholin <emtholin@gmail.com>
2018-02-24 20:32:39 +0100

    Pass Route match to the Component, bump @std/esm version in the example

--- example/server.js
+++ example/server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const { createHistory } = require('svelte-routing');
-require('svelte/ssr/register');
 const app = require('./App.html');
 
 const history = createHistory('memory');
