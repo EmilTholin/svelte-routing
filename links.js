15a28bfc51c284208d1b1a0bfb760545e18eb866
Christian Kaisermann <christian@kaisermann.me>
2018-05-16 18:40:20 -0300

    Make links.js support old browsers (#10)
    * Make links.js support old browsers
    
    Fixes #9
    
    * Fix indentation
    
    * Ignore anchors with target attribute

--- links.js
+++ links.js
@@ -24,9 +24,16 @@ export default function links(node) {
   }
 }
 
+function findClosest(tagName, el) {
+  while(el && el.tagName !== tagName) {
+    el = el.parentNode;
+  }
+  return el;
+}
+
 function onClick(event) {
-  const anchor = event.target.closest('a[href]:not([target])');
-  if (!anchor || event.button !== 0 || anchor.host !== location.host || isModifiedEvent(event) ||
+  const anchor = findClosest('A', event.target);
+  if (!anchor || anchor.target || event.button !== 0 || anchor.host !== location.host || isModifiedEvent(event) ||
     anchor.hasAttribute('noroute'))
   {
     return;
