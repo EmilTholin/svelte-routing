4ce52033602e801905fa45f88231bb27d5d81a9d
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-01-04 10:48:50 -0300

    Add semicolons

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,34 +1,34 @@
 declare module 'svelte-routing' {
-  import 'svelte2tsx/svelte-jsx'
-  import { SvelteComponent, SvelteComponentTyped } from 'svelte'
+  import 'svelte2tsx/svelte-jsx';
+  import { SvelteComponent, SvelteComponentTyped } from 'svelte';
   
   interface RouterProps {
-    basepath?: string
-    url?: string
+    basepath?: string;
+    url?: string;
   }
 
   interface RouteProps {
-    path?: string
-    component?: SvelteComponent
-    [additionalProp: string]: unknown
+    path?: string;
+    component?: SvelteComponent;
+    [additionalProp: string]: unknown;
   }
 
   interface RouteSlots {
     default: {
-      location: RouteLocation
-      params: RouteParams
-    }
+      location: RouteLocation;
+      params: RouteParams;
+    };
   }
 
   type State = {
-    [k in string | number]: unknown
+    [k in string | number]: unknown;
   }
 
   interface RouteLocation {
-    pathname: string
-    search: string
-    hash?: string
-    state: State
+    pathname: string;
+    search: string;
+    hash?: string;
+    state: State;
   }
 
   interface RouteParams {
@@ -36,21 +36,21 @@ declare module 'svelte-routing' {
   }
 
   interface LinkProps {
-    to: string
-    replace?: boolean
-    state?: State
-    getProps?: (linkParams: GetPropsParams) => Record<string, any>
+    to: string;
+    replace?: boolean;
+    state?: State;
+    getProps?: (linkParams: GetPropsParams) => Record<string, any>;
   }
 
   interface GetPropsParams {
-    location: RouteLocation
-    href: string
-    isPartiallyCurrent: boolean
-    isCurrent: boolean
+    location: RouteLocation;
+    href: string;
+    isPartiallyCurrent: boolean;
+    isCurrent: boolean;
   }
 
-	export const Router: SvelteComponentTyped<RouterProps>
-	export const Route: SvelteComponentTyped<RouteProps, Record<string, any>, RouteSlots>
+	export const Router: SvelteComponentTyped<RouterProps>;
+	export const Route: SvelteComponentTyped<RouteProps, Record<string, any>, RouteSlots>;
   export const Link: SvelteComponentTyped<
     Omit<
       LinkProps &
@@ -58,8 +58,8 @@ declare module 'svelte-routing' {
         svelte.JSX.SapperAnchorProps,
       "href"
     >
-  >
-	export const navigate: (to: string, { replace, state }: { replace?: boolean; state: State }) => void
-	export const link: (node: Element) => { destroy(): void }
-	export const links: (node: Element) => { destroy(): void }
+  >;
+	export const navigate: (to: string, { replace, state }: { replace?: boolean; state: State }) => void;
+	export const link: (node: Element) => { destroy(): void };
+	export const links: (node: Element) => { destroy(): void };
 }
